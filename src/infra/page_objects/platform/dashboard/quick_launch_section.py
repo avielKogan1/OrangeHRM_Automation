2214ff0decac6b7b7b9ed9cc664ded14c79fb2ce
avielKogan1 <kogan114@gmail.com>
2023-06-28 18:44:00 +0300

    more page objects

--- src/infra/page_objects/platform/dashboard/quick_launch_section.py
+++ src/infra/page_objects/platform/dashboard/quick_launch_section.py
@@ -2,12 +2,17 @@ from playwright.async_api import expect
 
 class QuickLaunchSection :
     def __init__(self, page) :
+        self.page = page
+        self.title = page.get_by_text("Quick Launch")
         self.assign_leave_button = page.get_by_role("button", name="Assign Leave")
         self.leave_list_button = page.get_by_role("button", name="Leave List")
         self.timesheets_button = page.get_by_role("button", name="Timesheets")
         self.apply_leave_button =page.get_by_role("button", name="Apply Leave")
         self.my_leave_button = page.get_by_role("button", name="My Leave")
         self.my_timesheets_button = page.get_by_role("button", name="My Timesheet")
+    
+    async def verify_section_loaded(self) :
+        await expect(self.title).to_be_visible()
     
     async def assign_leave(self) :
         await self.assign_leave.click()
